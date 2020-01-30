13855f4507e77a592daeafc6215d8e19ca08a0c6
jiushu <xpfwork@foxmail.com>
2020-01-31 06:09:18 +0800

    Initial commit

---
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.ixuea.courses.mymusic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ixuea.courses.mymusic.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
